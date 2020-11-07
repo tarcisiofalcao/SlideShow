7eb315dd482dcdaac379ea600fde46ce2ce96b37
Tarcísio Falcão <tfalcaof.eng@hotmail.com>
2020-11-07 15:13:42 -0300

    Ultimo Commit
    Finalizando os comandos e automatização do slider.

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let totalSlides = document.querySelectorAll('.slider-item').length;
 let slideWidth = `calc(100vw*${totalSlides})`;
 document.querySelector('.slider-width').style.width = slideWidth;
+let currentSlide = 0;
 
 function goPrev(){
-
+    currentSlide--;
+    if(currentSlide < 0){
+        currentSlide = totalSlides - 1;
+    }
+    updateMargin(currentSlide)
 }
 
 function goNext(){
-    
+    currentSlide++;
+    if(currentSlide == totalSlides){
+        currentSlide = 0;
+    }
+    updateMargin(currentSlide)
 }
+function updateMargin(n){
+    let newMargin = (n * 100);
+    document.querySelector('.slider-width').style.marginLeft = `-${newMargin}vw`
+}
+
+function passSlide(){
+    goNext()
+}
+setInterval(passSlide, 3000)

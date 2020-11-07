b410d9bda8db74bbc90d976020912c00065ee86c
Tarcísio Falcão <tfalcaof.eng@hotmail.com>
2020-11-06 22:18:26 -0300

    Iniciando funções
    Adicionando os primeiros comandos em js

---
+++ script.js
@@ -0,0 +1,11 @@
+let totalSlides = document.querySelectorAll('.slider-item').length;
+let slideWidth = `calc(100vw*${totalSlides})`;
+document.querySelector('.slider-width').style.width = slideWidth;
+
+function goPrev(){
+
+}
+
+function goNext(){
+    
+}
